74154d1d23c6885a155efda8ce2c5cb449ceb6bd
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-01-10 11:24:37 +0100

    modification des likes : like possible qu'une fois et changement de texte lorsqu'un like est fait.

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,6 +11,7 @@ export class FaceSnapComponent implements OnInit {
   createdDate!: Date;
   snaps!: number;
   imageURL!: string;
+  buttonText!: string;
 
   ngOnInit() {
     this.title = 'Archibald';
@@ -18,9 +19,16 @@ export class FaceSnapComponent implements OnInit {
     this.snaps = 6;
     this.imageURL =
       'https://pixabay.com/get/g1e713641af15abaa60dbb2a52d614eba37bc66557f1ab20088e2bbaa9288f9f1d148fb132f98493acd922950216cb0d4_640.jpg';
+    this.buttonText = 'Oh Snap !';
   }
 
-  onAddSnap() {
-    this.snaps++;
+  onSnap() {
+    if (this.buttonText === 'Oh Snap !') {
+      this.snaps++;
+      this.buttonText = 'Oop, UnSnap !';
+    } else {
+      this.snaps--;
+      this.buttonText = 'Oh Snap !';
+    }
   }
 }
