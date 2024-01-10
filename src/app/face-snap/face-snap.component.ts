9f0a0d5270ea1230279e78008f0e4b2be902c19d
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-01-10 10:58:40 +0100

    affichage de la card face-snap sur le html

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -10,10 +10,13 @@ export class FaceSnapComponent implements OnInit {
   description!: string;
   createdDate!: Date;
   snaps!: number;
+  imageURL!: string;
 
   ngOnInit() {
     this.title = 'Archibald';
     this.description = 'Mon meilleur ami';
     this.snaps = 6;
+    this.imageURL =
+      'https://pixabay.com/get/g1e713641af15abaa60dbb2a52d614eba37bc66557f1ab20088e2bbaa9288f9f1d148fb132f98493acd922950216cb0d4_640.jpg';
   }
 }
