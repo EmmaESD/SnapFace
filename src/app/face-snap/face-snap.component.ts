5f9c5f6aad8d24123e45965cb29e4f249e3e04cb
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-01-10 11:09:16 +0100

    ajout de la fonction like

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -19,4 +19,8 @@ export class FaceSnapComponent implements OnInit {
     this.imageURL =
       'https://pixabay.com/get/g1e713641af15abaa60dbb2a52d614eba37bc66557f1ab20088e2bbaa9288f9f1d148fb132f98493acd922950216cb0d4_640.jpg';
   }
+
+  onAddSnap() {
+    this.snaps++;
+  }
 }
