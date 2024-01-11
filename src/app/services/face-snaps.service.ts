6a385ac821794a913d469e89cfa92e5ff26bd453
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-01-11 14:10:23 +0100

    créations des routes du header et landing page

--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -11,7 +11,7 @@ export class FaceSnapsService {
       title: 'Archibald',
       description: 'Mon meilleur ami',
       imageURL:
-        'https://pixabay.com/get/g407660226a85ec39410f7b156445d7a3731a05df3190b1d56d953c41f0c395b39806a2faf22f360b10284a7dcee5b9c4d95958d9d585189321b057b99d264ef7_1280.jpg',
+        'https://pixabay.com/get/g6ca2af902452a2b2c997926331aeb7d46e704eb881ce6feb7016b576418efb0cef51d93025a4fbd07be69913da4a93db_640.jpg',
       createdDate: new Date(),
       snaps: 0,
       location: 'Paris',
